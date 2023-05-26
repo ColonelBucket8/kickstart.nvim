cf5f3770af651f7cd2d8bb16f4aa7f7225adc744
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-05-27 00:06:48 +0800

    Change from prettier to prettierd

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -64,7 +64,7 @@ return {
       null_ls.setup {
         sources = {
           null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.formatting.prettierd,
         },
       }
     end,
