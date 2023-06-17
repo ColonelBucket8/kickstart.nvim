14759da2f62270c89ed3e1c6bbae846cadcd13bb
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-06-17 10:15:50 +0800

    Update null ls

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -88,6 +88,8 @@ return {
           null_ls.builtins.formatting.prettierd,
           null_ls.builtins.formatting.black,
           null_ls.builtins.formatting.gofmt,
+          null_ls.builtins.formatting.clang_format,
+          null_ls.builtins.formatting.csharpier,
         },
 
         on_attach = function(client, bufnr)
