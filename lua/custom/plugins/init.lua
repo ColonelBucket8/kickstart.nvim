5d80799850961139f54a907ec5140e5cd08a7ece
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-07-12 22:17:10 +0800

    Add harpoon config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -170,4 +170,29 @@ return {
     end,
     requires = { 'nvim-tree/nvim-web-devicons' },
   },
+  {
+    'ThePrimeagen/harpoon',
+    config = function()
+      local mark = require 'harpoon.mark'
+      local ui = require 'harpoon.ui'
+
+      vim.keymap.set('n', '<leader>hi', ui.toggle_quick_menu, { desc = 'View all project marks' })
+      vim.keymap.set('n', '<leader>ha', mark.add_file, { desc = 'Mark file' })
+      vim.keymap.set('n', '<leader>h1', function()
+        ui.nav_file(1)
+      end, { desc = 'Go to mark index 1' })
+      vim.keymap.set('n', '<leader>h2', function()
+        ui.nav_file(2)
+      end, { desc = 'Go to mark index 2' })
+      vim.keymap.set('n', '<leader>h3', function()
+        ui.nav_file(3)
+      end, { desc = 'Go to mark index 3' })
+      vim.keymap.set('n', '<leader>h4', function()
+        ui.nav_file(4)
+      end, { desc = 'Go to mark index 4' })
+    end,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+  },
 }
