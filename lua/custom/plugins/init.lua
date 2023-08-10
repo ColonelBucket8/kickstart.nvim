42a5f0770c3421ed22f86b62443a2ce584a414a7
ColonelBucket8 <freedomterran89@gmail.com>
2023-08-10 21:40:56 +0800

    Add open api decrypt key

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -312,7 +312,7 @@ return {
     config = function()
       local home = vim.fn.expand '$HOME'
       require('chatgpt').setup {
-        -- api_key_cmd = 'gpg --decrypt ' .. home .. '/chatgpt.txt.gpg',
+        api_key_cmd = 'gpg --decrypt ' .. home .. '/.chatgpt.txt.gpg',
       }
     end,
     dependencies = {
