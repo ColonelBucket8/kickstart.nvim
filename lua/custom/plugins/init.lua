0697daf281527ce5e9b77027249d82fac5e5733f
ColonelBucket8 <freedomterran89@gmail.com>
2023-08-20 13:54:05 +0800

    Add solarized color theme

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -68,19 +68,19 @@ return {
           light = 'latte',
           dark = 'mocha',
         },
-        transparent_background = true, -- disables setting the background color.
-        show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
-        term_colors = false,           -- sets terminal colors (e.g. `g:terminal_color_0`)
+        transparent_background = false, -- disables setting the background color.
+        show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+        term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
-          enabled = false,             -- dims the background color of inactive window
+          enabled = false,              -- dims the background color of inactive window
           shade = 'dark',
-          percentage = 0.15,           -- percentage of the shade to apply to the inactive window
+          percentage = 0.15,            -- percentage of the shade to apply to the inactive window
         },
-        no_italic = false,             -- Force no italic
-        no_bold = false,               -- Force no bold
-        no_underline = false,          -- Force no underline
-        styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
-          comments = { 'italic' },     -- Change the style of comments
+        no_italic = false,              -- Force no italic
+        no_bold = false,                -- Force no bold
+        no_underline = false,           -- Force no underline
+        styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { 'italic' },      -- Change the style of comments
           conditionals = { 'italic' },
           loops = {},
           functions = {},
@@ -109,6 +109,7 @@ return {
       -- setup must be called before loading
       vim.cmd.colorscheme 'catppuccin'
     end,
+    enabled = false,
   },
   {
     'shaunsingh/nord.nvim',
@@ -334,5 +335,17 @@ return {
       { '<leader>gg', '<Cmd>Neogit<CR>', desc = 'Open LazyGit' },
     },
     config = true,
+  },
+  {
+    'Tsuzat/NeoSolarized.nvim',
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      local neo_solarized = require 'NeoSolarized'
+      neo_solarized.setup {
+        transparent = false,
+      }
+      vim.cmd [[ colorscheme NeoSolarized ]]
+    end,
   },
 }
