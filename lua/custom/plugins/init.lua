b49d1fde8f1dd6389685964bf3baaba27121e352
ColonelBucket8 <freedomterran89@gmail.com>
2023-08-28 21:34:39 +0800

    Add fold

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -16,8 +16,8 @@ vim.keymap.set('n', '<leader>.', '<Cmd>Ex<CR>')
 -- vim.o.foldmethod = 'expr'
 -- vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
 
-vim.o.foldmethod = 'indent'
-vim.o.foldlevel = 99
+-- vim.o.foldmethod = 'indent'
+-- vim.o.foldlevel = 99
 
 vim.cmd 'let g:netrw_banner = 0'
 
@@ -354,6 +354,34 @@ return {
     'Mofiqul/dracula.nvim',
     config = function()
       vim.cmd [[ colorscheme dracula ]]
+    end,
+  },
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = { 'kevinhwang91/promise-async' },
+    event = 'BufRead',
+    keys = {
+      {
+        'zR',
+        function()
+          require('ufo').openAllFolds()
+        end,
+      },
+      {
+        'zM',
+        function()
+          require('ufo').closeAllFolds()
+        end,
+      },
+    },
+    config = function()
+      vim.o.foldcolumn = '0'
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+      require('ufo').setup {
+        close_fold_kinds = { 'imports' },
+      }
     end,
   },
 }
